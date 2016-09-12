52d0fe3df804bd52f336cd16c623fe7020af5e3d
(cdep)illabout <cdep.illabout@gmail.com>
2016-09-12 09:12:23 +0900

    Playing around with the first proof.

--- what.v
+++ what.v
@@ -1,8 +1,10 @@
-Theorem my_first_proof : (forall A : Prop, A -> A).
+Theorem my_first_proof : (forall A : Prop, forall value_of_A: A, A).
 Proof.
   intros A.
-  intros proof_of_A.
-  exact proof_of_A.
+  (* intros proof_of_A.
+  exact proof_of_A. *)
+  intros value_of_A.
+  exact value_of_A.
 Qed.
 
 Theorem my_second_proof : (forall A : Prop, A -> A).
